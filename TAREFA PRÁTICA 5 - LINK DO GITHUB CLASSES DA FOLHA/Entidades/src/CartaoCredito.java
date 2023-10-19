a2d95fcd9269acc124e23182d310cd53989b1a8a
Lohan Parrilla <72171601+gohan-exe@users.noreply.github.com>
2023-10-18 23:51:13 -0300

    Update CartaoCredito.java

--- TAREFA PRÁTICA 5 - LINK DO GITHUB CLASSES DA FOLHA/Entidades/src/CartaoCredito.java
+++ TAREFA PRÁTICA 5 - LINK DO GITHUB CLASSES DA FOLHA/Entidades/src/CartaoCredito.java
@@ -7,7 +7,7 @@ public class CartaoCredito {
     boolean Cartao;
 
     void formaPagamento(){
-        if(tipos != null && senha == 4 && CVC == 3 && limite > 0){
+        if(tipos != null && senha == 9999 && CVC == 999 && limite > 0){
             Cartao = true;
             System.out.println("Pague seus impostos.");
         }
