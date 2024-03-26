0e7bcfdc778012518e48eb657b371f6fd0d94313
Lohan Parrilla <72171601+gohan-exe@users.noreply.github.com>
2024-03-25 23:00:52 -0300

    Update DemoApplication.java

--- TECNICAS DE PROGRAMAÇÃO II/ATIVIDADE PRÁTICA 3 - REDIS/demo/demo/src/main/java/atividade03/example/demo/DemoApplication.java
+++ TECNICAS DE PROGRAMAÇÃO II/ATIVIDADE PRÁTICA 3 - REDIS/demo/demo/src/main/java/atividade03/example/demo/DemoApplication.java
@@ -3,7 +3,6 @@ package atividade03.example.demo;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.exceptions.JedisConnectionException;
 import java.util.Map;
-import java.util.Set;
 import java.util.Scanner;
 
 public class DemoApplication {
@@ -21,7 +20,6 @@ public class DemoApplication {
 
     public void listarTarefas() {
         Map<String, String> tarefas = jedis.hgetAll("tarefas");
-        System.out.println("=================================");
         System.out.println("Lista de Tarefas:");
         for (Map.Entry<String, String> entry : tarefas.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
@@ -29,15 +27,25 @@ public class DemoApplication {
     }
 
     public void marcarTarefaConcluida(String id) {
-        jedis.hset("tarefas_concluidas", id, jedis.hget("tarefas", id));
-        jedis.hdel("tarefas", id);
+        String descricao = jedis.hget("tarefas", id);
+        if (descricao != null) {
+            jedis.hset("tarefas_concluidas", id, descricao);
+            jedis.hdel("tarefas", id);
+        } else {
+            System.out.println("ID de tarefa inválido.");
+        }
+    }
+
+    public void listarTarefasConcluidas() {
+        Map<String, String> tarefasConcluidas = jedis.hgetAll("tarefas_concluidas");
+        System.out.println("Lista de Tarefas Concluídas:");
+        for (Map.Entry<String, String> entry : tarefasConcluidas.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
     }
 
     public void removerTarefasConcluidas() {
-        Set<String> idsTarefasConcluidas = jedis.hkeys("tarefas_concluidas");
-        for (String id : idsTarefasConcluidas) {
-            jedis.hdel("tarefas_concluidas", id);
-        }
+        jedis.del("tarefas_concluidas");
     }
 
     public static void main(String[] args) {
@@ -47,20 +55,25 @@ public class DemoApplication {
         try {
             int opcao;
             do {
+                System.out.println("=================================");
                 System.out.println("\nEscolha uma opção:");
                 System.out.println("1. Adicionar Tarefa");
                 System.out.println("2. Listar Tarefas");
                 System.out.println("3. Marcar Tarefa Concluída");
-                System.out.println("4. Remover Tarefas Concluídas");
-                System.out.println("5. Sair");
+                System.out.println("4. Listar Tarefas Concluídas");
+                System.out.println("5. Remover Tarefas Concluídas");
+                System.out.println("6. Sair");
+                System.out.println("=================================");
 
                 opcao = scanner.nextInt();
                 scanner.nextLine();
 
                 switch (opcao) {
                     case 1:
+                        System.out.println("=================================");
                         System.out.println("Digite o ID da tarefa:");
                         String id = scanner.nextLine();
+                        System.out.println("=================================");
                         System.out.println("Digite a descrição da tarefa:");
                         String descricao = scanner.nextLine();
                         taskManager.adicionarTarefa(id, descricao);
@@ -69,20 +82,26 @@ public class DemoApplication {
                         taskManager.listarTarefas();
                         break;
                     case 3:
+                        System.out.println("=================================");
                         System.out.println("Digite o ID da tarefa concluída:");
                         String idConcluida = scanner.nextLine();
                         taskManager.marcarTarefaConcluida(idConcluida);
                         break;
                     case 4:
-                        taskManager.removerTarefasConcluidas();
+                        taskManager.listarTarefasConcluidas();
                         break;
                     case 5:
+                        taskManager.removerTarefasConcluidas();
+                        System.out.println("=================================");
+                        System.out.println("Tarefas concluídas removidas.");
+                        break;
+                    case 6:
                         System.out.println("Saindo...");
                         break;
                     default:
                         System.out.println("Opção inválida.");
                 }
-            } while (opcao != 5);
+            } while (opcao != 6);
         } catch (JedisConnectionException e) {
             System.err.println("Não foi possível conectar ao servidor Redis. Verifique suas credenciais e conexão com a internet.");
         } finally {
