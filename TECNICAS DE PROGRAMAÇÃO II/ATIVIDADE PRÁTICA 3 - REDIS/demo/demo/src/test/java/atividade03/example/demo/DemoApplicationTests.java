4a0b1b919de340148e20375f22c6029ce5b72d07
Lohan Parrilla <72171601+gohan-exe@users.noreply.github.com>
2024-03-25 21:57:58 -0300

    Add files via upload

---
+++ TECNICAS DE PROGRAMAÇÃO II/ATIVIDADE PRÁTICA 3 - REDIS/demo/demo/src/test/java/atividade03/example/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package atividade03.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = DemoApplication.class)
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
