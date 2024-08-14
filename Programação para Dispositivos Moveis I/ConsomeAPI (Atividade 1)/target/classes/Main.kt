2ba84928f9bc4c831006224da4eb7c459d529bed
Lohan Parrilla <72171601+gohan-exe@users.noreply.github.com>
2024-08-14 00:30:02 -0300

    Add files via upload

---
+++ Programação para Dispositivos Moveis I/ConsomeAPI (Atividade 1)/target/classes/Main.kt
@@ -0,0 +1,16 @@
+package br.edu.fatecpg
+
+import com.google.gson.Gson
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val scanner = Scanner(System.`in`)
+    println("Digite o CEP para consulta:")
+    val cep = scanner.nextLine()
+
+    val consumo = Consumo()
+    val endereco = consumo.buscaEndereco(cep)
+    
+    println(endereco)
+}
